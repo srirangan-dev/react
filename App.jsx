05aee834dc7b1dd01baca66af3be7d02d3b42d7b
srirangan <srirangan0603@gmail.com>
2026-01-27 21:34:08 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+import Course from './course';
+import sunrise from './assets/sunrise.jpg'
+import background from './assets/background.jpg'
+import CourseList from './Courselist';
+
+
+function App() {
+  
+
+  return (
+
+    <>
+<CourseList/>
+
+    </>
+    
+
+  );
+}  
+
+
+
+export default App;
