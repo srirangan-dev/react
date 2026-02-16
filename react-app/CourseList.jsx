f2e4195a131da032ec19b490e5e11dd5f5f813c1
Sri Rangan <srirangan0603@gmail.com>
2026-02-16 22:16:10 +0530

    Refactor CourseList with new courses and sorting
    Updated course details and added a new course. Sorted courses by rating and filtered them based on price.

--- react-app/CourseList.jsx
+++ react-app/CourseList.jsx
@@ -3,45 +3,66 @@ import Course from './course';
 import sunrise from './assets/sunrise.jpg'
 import background from './assets/background.jpg'
 
-
+    
 
 function CourseList(){
 
 
+
+  
+
       const courses = [
     {
-      name: 'HTML course',
-      price: '$199',
+
+        id:1,
+      name: 'html course',
+      price: 100,
       image: sunrise,
-      rating: 4,
+      rating: 2,
       show: true
     },
     {
-      name: 'CSS course',
-      price: '$199',
+
+        id:2,
+      name: 'css course',
+      price: 200,
       image: background,
+      rating: 3,
+      show: true
+    },
+    {
+        id:3,
+      name: 'js course',
+      price: 300,
+      image: sunrise,
       rating: 4,
       show: true
     },
     {
-      name: 'JS course',
-      price: '$199',
-      image: sunrise,
-      rating: 4,
-      show: true
+        id:4,
+      name: 'react course',
+      price: 600,
+      image: background,
+      rating: 5,
+      show:true
     }
   ];
 
 
+  courses.sort((x,y)=>y.rating-x.rating) //descending
+
+  const  vfmCourses = courses.filter((course)=>
+    course.price<=200
+
+  )
+
 
 
 const coursesList=courses.map(
-    (course)=>
-<Course name={course.name}
+    (course,index)=>
+<Course  key={index} name={course.name}
  image ={course.image} 
  price={course.price} rating={course.rating}/>
-
-
 )
 
 
