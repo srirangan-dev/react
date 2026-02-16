1c6ad4aa0eb375bc8f5065ca56a3c2bb3ea70905
Sri Rangan <srirangan0603@gmail.com>
2026-02-16 22:20:37 +0530

    Update console log in BuyCourse function
    Fix console log statement in BuyCourse function.

--- react-app/src/Course.jsx
+++ react-app/src/Course.jsx
@@ -8,7 +8,7 @@ function Course(props) {
 
   function BuyCourse(discount,e){
     console.log(props.name,"purchasesd",discount,"% discount");
-    console.log(e);
+    console.log(e);//evevnt
 
   }
 
@@ -18,8 +18,8 @@ function Course(props) {
         <img src={props.image} alt="" />
         <h3>{props.name}</h3>
         <p>{props.price}</p>
-       <button  onClick={(event)=>BuyCourse(20,event)}>Buy Now</button>
-      </div>
+       <button  onClick={(event)=>BuyCourse(20,event)}>Buy Now</button> 
+      </div> 
     );
 
   
